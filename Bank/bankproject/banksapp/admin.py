51888b7382f6df92fa23f75a7fd4afae434240d0
athulmathew02 <123078593+athulmathew02@users.noreply.github.com>
2023-04-14 10:11:37 +0530

    Delete Bank/bankproject directory

--- Bank/bankproject/banksapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . models import district
-from . models import branch
-
-# Register your models here.
-admin.site.register(district)
-admin.site.register(branch)
-
