77cf6e02b3d033b3fee3619f52895a3414ed88e9
athulmathew02 <123078593+athulmathew02@users.noreply.github.com>
2023-04-14 10:05:45 +0530

    Update urls.py

--- bankproject/banksapp/urls.py
+++ bankproject/banksapp/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from banksapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.HomePage, name='home'),
     path('register/', views.SignupPage, name='register'),
     path('login/', views.LoginPage, name='login'),
